eb8020145efec09f99bbd7f9242411d0ba59aba3
Vitor.Gomes <santosgomesv@gmail.com>
2022-03-25 13:15:10 -0300

    alterado a pagina inicial vai para pagina expecifi

--- produtos/urls.py
+++ produtos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('curso/<int:id>', views.curso, name = 'curso'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def home(request):
     Cursos = Curso.objects.all()
     return render(request,'home.html',{'Cursos':Cursos})
+
+def curso(request,id):
+    curso=Curso.objects.get(id=id)
+    return render(request,'curso.html',{'curso':curso})
