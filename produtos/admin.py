d2110f6e16768de582dc5cd6a601b303cc4872d6
Vitor.Gomes <santosgomesv@gmail.com>
2022-03-26 00:00:30 -0300

    finalizado todos os ajustes

--- produtos/admin.py
+++ produtos/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Curso, Imagem
 
 
+
+
+@admin.register(Imagem)
+class ImagemAdmin(admin.ModelAdmin):
+    list_display =('icone',)
+
 admin.site.register(Curso)
-admin.site.register(Imagem)
